c86961dbe548911bbf29849a087e4105bf5f9586
SpLiYvIiI <atukadagio123@gmail.com>
2020-12-20 18:22:55 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState } from 'react'
+import './styles.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App  = () =>{
+  const [allImages,setallImages] = useState([])
+  
+const updateImages = (e) => {
+	e.preventDefault()
+	const allNames = allImages.map(image => image.name)
+	const filesArray = Array.from(e.target.files).filter((file) =>{ return !allNames.includes(file.name)});
+	const toAdd = filesArray.map(file => {
+		return {
+			name : file.name,
+			url : URL.createObjectURL(file)
+		}
+	})
+	setallImages((prevImages) => prevImages.concat(toAdd));
+};
+const delPhoto = (photo)=>{
+	setallImages(allImages.filter(image => photo.name !== image.name))
+}
+  return(
+  <div>
+	<input type="file" multiple onClick = {({target})=>{target.value= ''}}onChange={updateImages}></input>
+	<div className="res">
+	{allImages.map((photo) => {
+		return (
+		<div key = {photo.url}>
+		<img src={photo.url} alt="" />
+		<div> 
+		{photo.name}
+		<button onClick={()=>{delPhoto(photo)}}>delete</button>
+		</div>
+		</div>
+		);
+	})}
+	</div>
+  </div>
+  )
 }
 
 export default App;
